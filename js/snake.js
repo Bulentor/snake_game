48e314dc245932999aa8e4f6913a9a10d372e9e9
root <root@LAPTOP-K9SIBRAJ.localdomain>
2026-04-16 01:06:24 +0300

    teleport

--- js/snake.js
+++ js/snake.js
@@ -22,8 +22,30 @@ const moveSnake = () => {
     newMovementSnake = {x: headSnake.x, y: headSnake.y + 1, d: direction, h: true}
   }
 
+  newMovementSnake = _setTeleportSnake(state.snake, newMovementSnake);
+
   state.snake.tail.shift();
   headSnake.h = false;
 
   state.snake.tail.push(newMovementSnake);
-}
+};
+
+const _setTeleportSnake = (snake, newHeadSnake) => {
+  const { direction } = snake;
+  const rowEdge = row - 1;
+
+  if (newHeadSnake.x > rowEdge && direction === 'right') {
+    return { ...newHeadSnake, x: 0 };
+  }
+  if (newHeadSnake.x < rowEdge && direction === 'left') {
+    return { ...newHeadSnake ,x: rowEdge };
+  }
+  if (newHeadSnake.y > rowEdge && direction === 'up') {
+    return { ...newHeadSnake, y: rowEdge };
+  }
+  if (newHeadSnake.y < rowEdge && direction === 'down') {
+    return { ...newHeadSnake, y: 0 };
+  }
+
+  return { ...newHeadSnake };
+};
