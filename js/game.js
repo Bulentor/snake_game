4bcab218308d014a0654b06c30ed15e435615690
root <root@LAPTOP-K9SIBRAJ.localdomain>
2026-04-15 22:53:38 +0300

    test

--- js/game.js
+++ js/game.js
@@ -5,14 +5,22 @@ const onload = () => {
   canvas.width = wigth;
   canvas.heigth = height;
 
+  function drawRectangle(x, y, color) {
+    ctx.beginPath();
+    ctx.rect(x * 30, y * 30, 30, 30);
+    ctx.fillStyle = color;
+    ctx.fill();
+    ctx.closePath();
+  }
+
   const rengerGame = () => {
     for(let y = 0; y < 20; y += 1){
       for(let x = 0; x < 20; x += 1){
-        //coordinate
-        ctx.fillStyle = 'red';
-        ctx.rect(x * 30, y * 30, 30, 30);
+        drawRectangle(x, y, 'red')
       }
     }
+
+    const renderScene = setInterval(render, 200);
   };
   renderGame();
 }
